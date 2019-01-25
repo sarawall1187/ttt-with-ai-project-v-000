37bc86fec6360064d450a8fab9bbbc6fd34d5376
Sara Walker <sarawall1187@gmail.com>
2019-01-25 21:25:55 +0000

    Automatically backed up by Learn

--- lib/game.rb
+++ lib/game.rb
@@ -70,11 +70,12 @@ class Game
    puts "Would you like to play '0', '1', or '2' players?"
    input = gets.strip
    if input == '0'
-     game.new(player_1=Players::Computer.new("X"), player_2=Players::Computer.new("O"), board=Board.new)
+     Game.new(player_1=Players::Computer.new("X"), player_2=Players::Computer.new("O"), board=Board.new)
      elsif input == '1'
-     game.new(player_1=Players::Human.new("X"), player_2=Players::Computer.new("O"), board=Board.new)
+     Game.new(player_1=Players::Human.new("X"), player_2=Players::Computer.new("O"), board=Board.new)
      elsif input == '2'
-     
+      Game.new(player_1=Players::Human.new("X"), player_2=Players::Human.new("O"), board=Board.new)
+     end
  end
  
 end
